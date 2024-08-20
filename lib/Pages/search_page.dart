5753f3ad89c087c2753f742a98868c5f86f4edc4
Abdulrahman <abdulrahmanabusnena01@gmail.com>
2024-08-20 03:18:43 +0200

    gti commit

--- lib/Pages/search_page.dart
+++ lib/Pages/search_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
@@ -10,6 +11,20 @@ class SearchPage extends StatefulWidget {
 class _SearchPageState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: const Color(0xffECE6CB),
+      appBar: AppBar(
+        backgroundColor: const Color(0xffECE6CB),
+        title: Text(
+          'where to ?',
+          style: GoogleFonts.inknutAntiqua(
+            color: Colors.black,
+            fontSize: 25,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        centerTitle: true,
+      ),
+    );
   }
 }
