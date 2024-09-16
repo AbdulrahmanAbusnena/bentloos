87869205a7d1fe088500315e9aa5608280fb3720
Abdulrahman Abusnena <130256608+AbdulrahmanAbusnena@users.noreply.github.com>
2024-09-16 19:55:31 +0200

    git commit

---
+++ lib/Components/container_homepage.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ContainerHomepage extends StatefulWidget {
+  const ContainerHomepage({super.key});
+
+  @override
+  State<ContainerHomepage> createState() => _ContainerHomepageState();
+}
+
+class _ContainerHomepageState extends State<ContainerHomepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 78,
+      width: 210,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(60),
+        color: Colors.grey[600],
+        //  border: Border.all(color: Colors.white),
+      ),
+      child: Row(
+        children: [
+          const SizedBox(
+            width: 10,
+          ),
+          const Icon(Icons.search, color: Colors.white),
+          const SizedBox(
+            width: 5,
+          ),
+          Text('Search for a pitch',
+              style: GoogleFonts.notoSansLao(
+                color: Colors.white,
+                fontSize: 18,
+                fontWeight: FontWeight.w600,
+              )),
+        ],
+      ),
+    );
+  }
+}
--- lib/Pages/homepage.dart
+++ lib/Pages/homepage.dart
@@ -1,3 +1,5 @@
+import 'package:bentlos/Components/container_homepage.dart';
+import 'package:bentlos/Components/container_homepage2.dart';
 import 'package:bentlos/Components/drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -30,13 +32,29 @@ class _HomepageState extends State<Homepage> {
                   },
                   icon: const Icon(Icons.menu)),
               const SizedBox(
-                width: 120,
+                width: 100,
               ),
               Text('Bentlos',
-                  style: GoogleFonts.mouseMemoirs(
+                  style: GoogleFonts.roboto(
                     color: Colors.black,
                     fontSize: 50,
+                    fontWeight: FontWeight.bold,
                   )),
+            ],
+          ),
+          const SizedBox(
+            height: 35,
+          ),
+          const Row(
+            children: [
+              SizedBox(
+                width: 5,
+              ),
+              ContainerHomepage(),
+              SizedBox(
+                width: 10,
+              ),
+              ContainerHomepag(),
             ],
           ),
         ],
